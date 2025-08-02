2141ff2d29e871d2ffce7c9a08b4751d793ba235
Stefan Morrell <38956076+Tromador@users.noreply.github.com>
2025-08-02 14:13:19 +0100

    Add logging options
    Either log to arbitrary path or syslog.

--- include/config.h
+++ include/config.h
@@ -10,4 +10,10 @@
 /* Socket permissions: will be set by systemd in practice */
 #define SOCKET_MODE 0660
 
+/* Optional log file: if empty string, use syslog */
+#define LOG_FILE_PATH ""
+
+/* Syslog facility: used if LOG_FILE_PATH is empty */
+#define LOG_FACILITY LOG_MAIL
+
 #endif /* CONFIG_H */
